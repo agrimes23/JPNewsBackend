a894ff0435653bc47f80262946fb1406cbb31e47
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-09-09 00:04:13 -0500

    deleted some console logs

--- newsRoutes.js
+++ newsRoutes.js
@@ -5,14 +5,12 @@ require('dotenv').config();
 
 router.get('/news', async (req, res) => {
 
-    console.log("Well hiii")
     try {
         console.log("Request to /news received");
         const response = await axios.get(
             `https://newsapi.org/v2/top-headlines?country=jp&apiKey=${process.env.NEWS_API_KEY}`
         );
         const newsData = response.data;
-        console.log("ooo json data: " + JSON.stringify(newsData))
         res.json(newsData);
     } catch (error) {
         console.error('Error fetching news data:', error);

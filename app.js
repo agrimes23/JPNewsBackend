cb1323e225b41ecbedb8cb0dce36f5a3080c098a
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-09-08 23:32:21 -0500

    backend works!

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000;
+const cors = require("cors")
+
+
+const newsRoutes = require('./newsRoutes');
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hellow, Express!')
 })
+
+app.use('/api', newsRoutes)
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
--- newsRoutes.js
+++ newsRoutes.js
@@ -1,15 +1,20 @@
 const express = require("express");
-const axios = require("axois");
+const axios = require("axios");
 const router = express.Router();
 
 router.get('/news', async (req, res) => {
+
+    console.log("Well hiii")
     try {
+        console.log("Request to /news received");
         const response = await axios.get(
-            `https://newsapi.org/v2/top-headlines?country=jp&apiKey=${process.env.NEW_API_KEY}`
+            `https://newsapi.org/v2/top-headlines?country=jp&apiKey=${process.env.NEWS_API_KEY}`
         );
         const newsData = response.data;
+        console.log("ooo json data: " + JSON.stringify(newsData))
         res.json(newsData);
     } catch (error) {
+        console.error('Error fetching news data:', error);
         res.status(500).json({ error: 'Unable to fetch news data'});
     }
 });

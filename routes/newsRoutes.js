e9a83d180067eb4bb7449fc6709c2cce558ac6f2
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-09-14 15:52:24 -0500

    tried it with the actual api, it works! Now, will send it to the frontend and try to style it there

--- routes/newsRoutes.js
+++ routes/newsRoutes.js
@@ -12,35 +12,43 @@ router.get('/news', async (req, res) => {
         const response = await axios.get(
             `https://newsapi.org/v2/top-headlines?country=jp&apiKey=${process.env.NEWS_API_KEY}`
         );
-
-        const testResponse = "ウクライナ軍は、１３日未明にクリミア半島の港湾都市セバストポリにあるロシア海軍指の資産および港湾インフラに「手袋」対する攻撃を実施し、成功したとテレグラムで発表した。最大規模の攻撃という 。"
     
         const n4data = JSON.parse(fs.readFileSync('./db/n4.json', 'utf8'));
 
-        performSearch(testResponse, n4data)
+        performSearch(response.data.articles, n4data)
 
 ////////////////////////////////////////////////////////////////////////////////
     // Implement your search logic here (e.g., using lodash or custom code)
     function performSearch(newsDescData, searchWords) {
         const searchResults = [];
+        let parsedData = ""
     
         // Loop through each search word
-        searchWords.forEach((n4Word) => {
-            const pattern = new RegExp(n4Word.word, "i");
-    
-            // Search for the word in the news description using the regular expression pattern
-            const matchingWord = newsDescData.match(pattern);
-    
-            if (matchingWord) {
-                searchResults.push(matchingWord[0]); // Push the matched word to the results array
+        newsDescData.forEach((article) => {
+            if(article.description) {
+            searchWords.forEach((n4Word, index) => {
+                const pattern = new RegExp(n4Word.word, "ig");
+        
+                // Search for the word in the news description using the regular expression pattern
+                const matchingWord = article.description.match(pattern);
+        
+                if (matchingWord) {
+                    // searchResults.push(matchingWord[0]); // Push the matched word to the results array
+                    article.description = article.description.replace(matchingWord, `<span id=${index} className="text-red-500 font-bold">${matchingWord}</span>`)
+
+                    // `<span id=${index} className="text-red-500 font-bold">${matchingWord}</span>`
             }
-        });
+        
+            });
     
-        console.log("all matching words in news desc: " + JSON.stringify(searchResults));
+            console.log("did it replace word with <span>?: " + JSON.stringify(article.description));
+        } else {
+            console.log("article desc is null, skipping")
+        }
+        })
     }
 ////////////////////////////////////////////////////////////////////////////////  
 
-        const newsData = response.data;
         res.json(newsData);
     } catch (error) {
         console.error('Error fetching news data:', error);

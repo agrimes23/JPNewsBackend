b90c56a2ecfa0983fcd6d80e92d3be73ceeceb85
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-10-02 22:05:19 -0500

    gets matching words from news desc based on level submitted on frontend

--- routes/kanjiRoutes.js
+++ routes/kanjiRoutes.js
@@ -3,30 +3,24 @@ const router = express.Router();
 const fs = require('fs');
 const axios = require("axios");
 
-// Making this so it gets the news descriptions on the backend
-// it will:
-  // loop through each word of the description, 
-  // if there's a word that is the same as a word in n4.json, replace that word with <span id={} className="text-red-500 bold"></span>
-// send the array to the frontend, where it then can display on screen
-
 // Post-MPV:
   // when word is clicked: have another route that gets the word's definition, makes an api call to get it
   
 
-router.get('/search', async (req, res) => {
+router.get('/search/:level', async (req, res) => {
 
+  let level = req.params.level
   try {
     console.log("Request to /search received, but have commented everything out :D");
 
     const response = await axios.get(
       `https://newsapi.org/v2/top-headlines?country=jp&apiKey=${process.env.NEWS_API_KEY}`
   );
-    // Read the large N4 JSON file
-    const n4data = JSON.parse(fs.readFileSync('./db/n4.json', 'utf8'));
-    // Perform the search logic here
 
-    console.log("response.data: " + JSON.stringify(response.data))
-    performSearch(response.data.articles, n4data)
+    const jlptLeveldata = JSON.parse(fs.readFileSync(`./db/${level}.json`, 'utf8'));
+
+    // console.log("response.data: " + JSON.stringify(response.data))
+    performSearch(response.data.articles, jlptLeveldata)
 
 
   } catch (err) {
@@ -59,7 +53,6 @@ router.get('/search', async (req, res) => {
     res.json(searchResults) 
   
   }
-
 
 });
 

3889caf8af48dcee06170bf141d3ea3c40679b4a
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-10-22 21:19:53 -0500

    halfway through graphql tutorial

--- graphql/graphql.js
+++ graphql/graphql.js
@@ -1,17 +1,17 @@
-const { ApolloServer, gql } = require('apollo-server-express');
-const { typeDefs, resolvers } = require('./your-graphql-schema'); // Import your GraphQL schema and resolvers
-const express = require('express');
+// const { ApolloServer, gql } = require('apollo-server-express');
+// const { typeDefs, resolvers } = require('./your-graphql-schema'); // Import your GraphQL schema and resolvers
+// const express = require('express');
 
-const server = new ApolloServer({ typeDefs, resolvers });
+// const server = new ApolloServer({ typeDefs, resolvers });
 
-// Create an instance of Express
-const app = express();
+// // Create an instance of Express
+// const app = express();
 
-server.applyMiddleware({ app, path: '/graphql' });
+// server.applyMiddleware({ app, path: '/graphql' });
 
-// ... The rest of your existing app configuration ...
+// // ... The rest of your existing app configuration ...
 
-// Start your Express server
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+// // Start your Express server
+// app.listen(port, () => {
+//     console.log(`Server is running on port ${port}`);
+// });
--- graphql/graphqlSchema.js
+++ graphql/graphqlSchema.js
@@ -1,60 +1,79 @@
-const { GraphQLObjectType, GraphQLSchema, GraphQLID, GraphQLString, GraphQLList, GraphQLNonNull } = require('graphql');
-const User = require('./models/userModel'); // Import your Mongoose model
+// const { GraphQLObjectType, GraphQLSchema, GraphQLID, GraphQLString, GraphQLList, GraphQLNonNull } = require('graphql');
+// const User = require('./models/userModel'); // Import your Mongoose model
 
-const UserType = new GraphQLObjectType({
-  name: 'User',
-  fields: () => ({
-    _id: { type: GraphQLID },
-    name: { type: GraphQLString },
-    email: { type: GraphQLString },
-    createdAt: { type: GraphQLString },
-    updatedAt: { type: GraphQLString },
-  }),
-});
+// const UserType = new GraphQLObjectType({
+//   name: 'User',
+//   fields: () => ({
+//     _id: { type: GraphQLID },
+//     name: { type: GraphQLString },
+//     email: { type: GraphQLString },
+//     createdAt: { type: GraphQLString },
+//     updatedAt: { type: GraphQLString },
+//   }),
+// });
 
-const RootQuery = new GraphQLObjectType({
-  name: 'RootQueryType',
-  fields: {
-    getUser: {
-      type: UserType,
-      args: { _id: { type: GraphQLID } },
-      resolve(parent, args) {
-        return User.findById(args._id);
-      },
-    },
-    getAllUsers: {
-      type: new GraphQLList(UserType),
-      resolve() {
-        return User.find({});
-      },
-    },
-  },
-});
+// const RootQuery = new GraphQLObjectType({
+//   name: 'RootQueryType',
+//   fields: {
+//     getUser: {
+//       type: UserType,
+//       args: { _id: { type: GraphQLID } },
+//       resolve(parent, args) {
+//         return User.findById(args._id);
+//       },
+//     },
+//     getAllUsers: {
+//       type: new GraphQLList(UserType),
+//       resolve() {
+//         return User.find({});
+//       },
+//     },
+//   },
+// });
 
-const Mutation = new GraphQLObjectType({
-  name: 'Mutation',
-  fields: {
-    createUser: {
-      type: UserType,
-      args: {
-        name: { type: new GraphQLNonNull(GraphQLString) },
-        email: { type: new GraphQLNonNull(GraphQLString) },
-        password: { type: new GraphQLNonNull(GraphQLString) },
-      },
-      resolve(parent, args) {
-        const user = new User({
-          name: args.name,
-          email: args.email,
-          password: args.password,
-        });
-        return user.save();
-      },
-    },
-    // Add update and delete mutations here
-  },
-});
+// const Mutation = new GraphQLObjectType({
+//   name: 'Mutation',
+//   fields: {
+//     createUser: {
+//       type: UserType,
+//       args: {
+//         name: { type: new GraphQLNonNull(GraphQLString) },
+//         email: { type: new GraphQLNonNull(GraphQLString) },
+//         password: { type: new GraphQLNonNull(GraphQLString) },
+//       },
+//       resolve(parent, args) {
+//         const user = new User({
+//           name: args.name,
+//           email: args.email,
+//           password: args.password,
+//         });
+//         return user.save();
+//       },
+//     },
+//     // Add update and delete mutations here
+//   },
+// });
 
-module.exports = new GraphQLSchema({
-  query: RootQuery,
-  mutation: Mutation,
-});
+// module.exports = new GraphQLSchema({
+//   query: RootQuery,
+//   mutation: Mutation,
+// });
+
+const gql = require("graphql-tag")
+
+const typeDefs = gql`
+
+type Query {
+  userAuth: [User!]!
+}
+
+"User is for user authnetication"
+type User {
+  name: String!
+  email: String!
+  password: String!
+}
+
+`;
+
+module.exports = typeDefs
